a36112960637262dc347be152290571f5f846368
Hung Mai <hung.mai@asnet.com.vn>
2024-09-23 17:32:44 +0700

    Update storybook

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -94,11 +94,6 @@ export const mockProject: IProject[] = [
     views: 81532
   }
 ];
-export const navItems = [
-  { name: 'Home', url: END_POINT.HOME },
-  { name: 'Places', url: END_POINT.PLACES },
-  { name: 'Products', url: END_POINT.PRODUCTS },
-  { name: 'Projects', url: END_POINT.PROJECTS },
-  { name: 'Portfolio', url: END_POINT.PORTFOLIO },
-  { name: 'About Us', url: END_POINT.ABOUT_US }
-];
+
+export const mockCategories = ['CategoryA', 'CategoryB', 'CategoryC', 'CategoryD']
+
